a9379a9d5fb791ddc129f6fdc89279a31dfa4097
ANZHELIKA <angela.pikulina@gmail.com>
2023-10-22 21:20:45 +0200

    HW-2-1-4

--- task_score.js
+++ task_score.js
@@ -10,8 +10,11 @@ const averageScore = (introductionScore + gitScore + jsScore) / 3;
 const firstName = "Anzhelika";
 const lastName = "Pikulina";
 
+// Shortening the name to the first letter
+const abbreviatedName = firstName.slice(0, 1);
+
 // Outputting the result to the console using template strings
-console.log(`Student: ${firstName} ${lastName}`);
+console.log(`Student: ${abbreviatedName}. ${lastName}`);
 console.log(`Points for the module "Introduction": ${introductionScore}`);
 console.log(`Points for the module "Git": ${gitScore}`);
 console.log(`Points for the module "JS": ${jsScore}`);
